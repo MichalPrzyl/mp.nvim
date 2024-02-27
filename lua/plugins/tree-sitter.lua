5b37b73e9f26805d9fe4a6c963430437609ac8a0
michalPrzyl <michal.przyl@gmail.com>
2024-02-27 23:54:07 +0100

    Add Tree-sitter text-object select indent
    Using Ctrl-Space to increment selection.
    Using Backspace to decrement selection.

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,12 +1,22 @@
 return {
-  "nvim-treesitter/nvim-treesitter", build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local config = require("nvim-treesitter.configs")
 
-    config.setup({
-      ensure_installed = { "lua", "vim", "javascript", "python" },
-      highlight = { enable = true },
-      indent = { enable = true },
-     })
-  end
+		config.setup({
+			ensure_installed = { "lua", "vim", "javascript", "python" },
+			highlight = { enable = true },
+			indent = { enable = true },
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
+		})
+	end,
 }
