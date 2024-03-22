835de24d5510b91ab7cc918ccc5a61ce7ef093ea
michalPrzyl <michal.przyl@gmail.com>
2024-03-22 23:52:10 +0100

    Add recent files plugin
    Keymap:
    `<leader>`rec

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,15 +1,19 @@
 return {
-  'nvim-telescope/telescope.nvim', tag = '0.1.5',
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.5",
 
-  dependencies = {
-    {'nvim-lua/plenary.nvim' },
-    {"nvim-telescope/telescope-live-grep-args.nvim"},
-  },
+	dependencies = {
+		{ "nvim-lua/plenary.nvim" },
+		{ "nvim-telescope/telescope-live-grep-args.nvim" },
+		{ "smartpde/telescope-recent-files" },
+	},
 
-  config = function()
-    local builtin = require("telescope.builtin")
+	config = function()
+		local builtin = require("telescope.builtin")
+		require("telescope").load_extension("live_grep_args")
+		require("telescope").load_extension("recent_files")
 
-    vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-  end
+		vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+		vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+	end,
 }
