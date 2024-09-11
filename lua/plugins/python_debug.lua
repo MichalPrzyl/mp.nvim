5855a3fe7518da1b89a3933d4fc1d32df32ae8aa
Michał Przyłucki <michal.przyl@gmail.com>
2024-09-11 20:42:59 +0200

    Don't repeat yourself

--- lua/plugins/python_debug.lua
+++ lua/plugins/python_debug.lua
@@ -15,7 +15,7 @@ return {
 					type = "python",
 					request = "launch",
 					name = "Launch Program",
-					program = "script.py", -- python file to debug 
+					program = "script.py", -- python file to debug
 					pythonPath = function()
 						return "/home/michal/Projects/django-nvim-debugger/venv/bin/python3.12"
 					end,
@@ -26,6 +26,7 @@ return {
 	-- requires = {
 	{
 		"rcarriga/nvim-dap-ui",
+		requires = { "mfussenegger/nvim-dap" }, -- upewnij się, że jest zależność od 'nvim-dap'
 		config = function()
 			-- Inicjalizacja nvim-dap-ui
 			require("dapui").setup()
@@ -48,25 +49,9 @@ return {
 	},
 	{
 		"theHamsta/nvim-dap-virtual-text",
+		requires = { "mfussenegger/nvim-dap" }, -- upewnij się, że jest zależność od 'nvim-dap'
 		config = function()
-			-- Inicjalizacja nvim-dap-ui
-			require("dapui").setup()
-
-			-- Automatyczne otwieranie i zamykanie UI podczas debugowania
-			local dap, dapui = require("dap"), require("dapui")
-			dap.listeners.after.event_initialized["dapui_config"] = function()
-				dapui.open()
-			end
-			dap.listeners.before.event_terminated["dapui_config"] = function()
-				dapui.close()
-			end
-			dap.listeners.before.event_exited["dapui_config"] = function()
-				dapui.close()
-			end
-
-			-- Inicjalizacja nvim-dap-virtual-text
 			require("nvim-dap-virtual-text").setup()
 		end,
 	},
-	-- },
 }
